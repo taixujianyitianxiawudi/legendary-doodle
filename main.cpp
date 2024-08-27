7bb4bbb49875ee865a52c3f6f30a378a2c003df5
taixujianyitianxiawudi <408707751@qq.com>
2024-08-27 12:57:22 +0800

    first

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <vulkan/vulkan.h>
+#include <iostream>
+
+
+int main()
+{
+    VkApplicationInfo appInfo = {};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Hello Triangle";
+    appInfo.pEngineName = "No Engine";
+
+    VkInstanceCreateInfo instanceInfo = {};
+    instanceInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    instanceInfo.pApplicationInfo = &appInfo;
+
+    VkInstance instance;
+
+    VkResult result =  vkCreateInstance(&instanceInfo, 0, &instance);
+    if (result == VK_SUCCESS) {
+        std::cout << "Success" << std::endl;
+    }
+    return 0;
+}
